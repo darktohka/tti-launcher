a7822c30d4c6df19d444e2214947f9ee68e82701
Disyer <daniel@tohka.us>
2026-09-05 16:48:55 +0300

    Add window icon

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "launcherconstants.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
@@ -9,7 +10,14 @@ int main(int argc, char *argv[])
     QApplication::setApplicationName(QStringLiteral("Toontown Infinite"));
     QApplication::setApplicationVersion(launcher::kVersion);
 
+    // The PNG provides a fallback on platforms where the ICO decoder is unavailable.
+    QIcon appIcon;
+    appIcon.addFile(QStringLiteral(":/icons/launcher.ico"));
+    appIcon.addFile(QStringLiteral(":/icons/launcher_256.png"));
+    QApplication::setWindowIcon(appIcon);
+
     LauncherWindow window;
+    window.setWindowIcon(appIcon);
     window.show();
 
     return app.exec();
